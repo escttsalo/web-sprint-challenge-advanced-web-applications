ab872b49862bc622daecc104cec0470546b9fcfc
escttsalo <escttsalo@gmail.com>
2021-06-18 16:36:06 -0600

    tests

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -3,15 +3,41 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
+const color = {
+    color: "aliceblue",
+    code: {
+        hex: "#f0f8ff",
+    },
+    id: 1,
+}
+
 test("Renders without errors with blank color passed into component", () => {
+    render(<Color color={ {color:'blank', code:{hex: '#ffffff'}, id:1} }/>)
 });
   
 test("Renders the color passed into component", () => {
+    render(<Color color={color}/>)
 });
 
 test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
+    const toggleEdit = jest.fn()
+    const deleteColor = jest.fn()
+    render(<Color color={color} deleteColor={deleteColor} toggleEdit={toggleEdit} />)
+
+    const button = screen.queryByTestId('delete')
+    userEvent.click(button)
+
+    expect(toggleEdit).toHaveBeenCalledTimes(1)
 });
 
 test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
+    const setEditColor = jest.fn()
+    const toggleEdit = jest.fn()
+    render(<Color color={color} toggleEdit={toggleEdit} setEditColor={setEditColor}/>)
+
+    const div = screen.getByTestId('color')
+    userEvent.click(div)
+
+    expect(toggleEdit).toHaveBeenCalledTimes(1)
+    expect(setEditColor).toHaveBeenCalledTimes(1)
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -2,11 +2,31 @@ import React from 'react';
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
+const colors = [
+    {
+        color: "aliceblue",
+        code: {
+            hex: "#f0f8ff",
+        },
+        id: 1,
+    }
+]
+
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={[]}/>)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList colors={colors}/>)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const { rerender } = render(<ColorList colors={colors} editing={true}/>)
+
+    const editMenu = screen.queryByTestId('editMenu')
+    expect(editMenu).toBeInTheDocument();
+
+    rerender(<ColorList colors={colors} editing={false}/>)
+
+    expect(editMenu).not.toBeInTheDocument();
 });
